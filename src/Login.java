81a5623ea4b8e0ea6a6bdafef2c5c881ab1a9a25
amitgh12 <cmckay473@student.msjc.edu>
2021-06-27 11:50:26 +0530

    service

---
+++ src/Login.java
@@ -0,0 +1,32 @@
+import java.io.*;  
+import javax.servlet.*;  
+import javax.servlet.http.*;  
+  
+  
+public class Login extends HttpServlet {  
+	private static final long serialVersionUID = 1L;
+public void doPost(HttpServletRequest request, HttpServletResponse response)  
+        throws ServletException, IOException {  
+  
+    response.setContentType("text/html");  
+    PrintWriter out = response.getWriter();  
+          
+    String n=request.getParameter("userName");  
+    String p=request.getParameter("userPass");  
+           
+	
+    if(p.equals("test"))
+	{  
+        RequestDispatcher rd=request.getRequestDispatcher("servlet2");  
+        rd.forward(request, response);  
+    } 	
+		
+    else{  
+        out.print("Sorry UserName or Password Error!");  
+        RequestDispatcher rd=request.getRequestDispatcher("/index.html");  
+        rd.include(request, response);  
+                      
+        }  
+    }  
+  
+}  
---
+++ src/WelcomeServlet.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import javax.servlet.*;  
+import javax.servlet.http.*;  
+  
+public class WelcomeServlet extends HttpServlet {  
+	private static final long serialVersionUID = 1L;
+    public void doPost(HttpServletRequest request, HttpServletResponse response)  
+        throws ServletException, IOException {  
+  
+    response.setContentType("text/html");  
+    PrintWriter out = response.getWriter();  
+          
+    String n=request.getParameter("userName");  
+    out.print("Welcome "+n);  
+    }  
+  
+}  
